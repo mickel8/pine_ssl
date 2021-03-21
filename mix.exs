c94e96f4a48956e4e98f06aaabf96d3441f08a67
Michał Śledź <sledzson08@gmail.com>
2021-03-21 16:25:00 +0100

    Add rustler

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule PineSsl.MixProject do
       app: :pine_ssl,
       version: "0.1.0",
       elixir: "~> 1.11",
+      compilers: [:rustler] ++ Mix.compilers(),
+      rustler_crates: [pinessl: []],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +23,7 @@ defmodule PineSsl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.21.1"}
     ]
   end
 end
